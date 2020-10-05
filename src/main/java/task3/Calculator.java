2dde080a6619ff7c32c2e6d545c8792954d50458
sentidas <sentidas@yandex.ru>
2020-10-05 10:16:03 +0300

    correct task 3

--- src/main/java/task3/Calculator.java
+++ src/main/java/task3/Calculator.java
@@ -11,25 +11,27 @@ public class Calculator {
     double number1 = scanner.nextDouble();
     System.out.println("введите второе число");
     double number2 = scanner.nextDouble();
-
     System.out.println("введите знак операции, допускается ввод операций: сложение '+'," +
             " вычитание '-', умножение '*', деление '/'");
-    Scanner scanner1 = new Scanner(System.in);
-    String symbol = scanner1.nextLine();
+    char symbol = scanner.next().charAt(0);
 
-    if (symbol.equals("+")) {
-      System.out.printf("Cумма чисел: %.4f", number1 + number2); // вычисление суммы
-    } else if (symbol.equals( "-")) {
-      System.out.printf("Разность чисел: %.4f", number1 - number2); // вычисление разности
-    } else if (symbol.equals( "/")) {
-      System.out.printf("Частное чисел: %.4f", number1 / number2); // вычисление частного
-    } else if (symbol.equals( "*")) {
-      System.out.printf("Произведение чисел: %.4f", number1 * number2); // вычисление произведения
-    } else
-      System.out.println("неизвестная операция, введите снова");
+    switch (symbol) {
+      case '+':
+        System.out.printf("Cумма чисел: %.4f", number1 + number2); // вычисление суммы
+        break;
+      case '-':
+        System.out.printf("Разность чисел: %.4f", number1 - number2); // вычисление разности
+        break;
+      case '/':
+        System.out.printf("Частное чисел: %.4f", number1 / number2); // вычисление частного
+        break;
+      case '*':
+        System.out.printf("Произведение чисел: %.4f", number1 * number2); // вычисление произведения
+        break;
+      default:
+        System.out.println("неизвестная операция, введите снова");
 
-    scanner.close();
-    scanner1.close();
+        scanner.close();
+    }
   }
-
 }
